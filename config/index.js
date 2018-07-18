3f14b0213812a8a8c00b85c3f1ae2e23986a6a27
heyong_ <heyonghappy@126.com>
2018-07-18 18:39:45 +0800

    changed

--- config/index.js
+++ config/index.js
@@ -65,18 +65,18 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
-  },
-  proxyTable: {
-   '/json': {
-    target: 'http://127.0.0.1:3006',
-    changeOrigin: true,
-    pathRewrite: {
-     '^/json': '/json'
-    }
-   }
   }
-    proxy: {
-    host: '127.0.0.1',
-    port: 3006
-  },
+  // proxyTable: {
+  //  '/json': {
+  //   target: 'http://127.0.0.1:3006',
+  //   changeOrigin: true,
+  //   pathRewrite: {
+  //    '^/json': '/json'
+  //   }
+  //  }
+  // }
+  //   proxy: {
+  //   host: '127.0.0.1',
+  //   port: 3006
+  // },
 }
