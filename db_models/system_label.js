56c6362e51047377b204aaf068b551f7ec0a7252
heyong_ <heyonghappy@126.com>
2018-07-31 19:09:02 +0800

    123

---
+++ db_models/system_label.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize'),
+  mySequelize = require('../db/ormconfig.js')
+
+
+  const System_label = mySequelize.define('system_label',{
+
+  })
+
+  module.exports=System_label
